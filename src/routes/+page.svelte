4711e5f9ea18bbae4b59b924ebbebebfb8f1fddc
Sean Behan <codebam@riseup.net>
2023-10-23 17:05:11 -0400

    change to onmount to fix rerender

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { beforeUpdate } from 'svelte';
+	import { onMount } from 'svelte';
 	export let data: {posts: {path: string, meta: {title: string, date: string}}[]};
 	let response: string = "I’m a full stack developer. I code in TypeScript, Rust, C#, and Python. I have a passion for open source and Linux.";
-	beforeUpdate(async () => {
+	onMount(async () => {
 		const json = await fetch("https://cloudflare-ai-api.codebam.workers.dev/api/question/custom", { method: "POST", body: JSON.stringify({system: ["your job is to explain who Sean Behan is", `use seed ${crypto.randomUUID()}`, "Sean Behan is a programmer who codes in TypeScript, Rust, C#, Python", "Sean Behan has a passion for open source and Linux"], user: ["tell me about Sean Behan"]})}).then((resp) => resp.json());
 		response = json.response;
 	});
