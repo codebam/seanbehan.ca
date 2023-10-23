46fb368e4181315017eb5ccf5efa7cfe1694e999
Sean Behan <codebam@riseup.net>
2023-10-23 15:58:45 -0400

    add publickey

--- src/lib/components/TopBar.svelte
+++ src/lib/components/TopBar.svelte
@@ -1,6 +1,6 @@
 <style>
   header {
-    display: flex;
+    text-align: center;
     justify-content: space-around;
   }
 </style>
@@ -9,5 +9,7 @@
   <h1>Sean Behan</h1>
   <a href="/"><h2>Home</h2></a>
   <a href="/posts"><h2>Posts</h2></a>
+  <a href="/playlists"><h2>Playlists</h2></a>
   <a href="https://github.com/codebam"><h2>GitHub</h2></a>
+  <a href="/contact"><h2>Contact</h2></a>
 </header>
