df27abe9d9e41ff111eb58c5b605bbdf8178d65b
Sean Behan <codebam@riseup.net>
2023-10-23 21:02:38 -0400

    add sitemap.xml

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,41 @@
+import getPosts from '$lib/getPosts';
+
+export const GET = async () => {
+	const posts = await getPosts();
+	console.log(posts);
+	const body = render(posts);
+	const headers = { 'Content-Type': 'application/xml' };
+	return new Response(body, { headers });
+};
+
+const render = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url>
+<loc>https://seanbehan.ca</loc>
+<lastmod>${new Date().toUTCString()}</lastmod>
+</url>
+<url>
+<loc>https://seanbehan.ca/contact</loc>
+<lastmod>${new Date().toUTCString()}</lastmod>
+</url>
+<url>
+<loc>https://seanbehan.ca/posts</loc>
+<lastmod>${new Date().toUTCString()}</lastmod>
+</url>
+<url>
+<loc>https://seanbehan.ca/resume</loc>
+<lastmod>${new Date().toUTCString()}</lastmod>
+</url>
+<url>
+<loc>https://seanbehan.ca/playlists</loc>
+<lastmod>${new Date().toUTCString()}</lastmod>
+</url>
+${posts
+	.map(
+		(post) => `<url>
+<loc>https://seanbehan.ca${post.path}</loc>
+<lastmod>${new Date(post.meta.date).toUTCString()}</lastmod>
+</url>`
+	)
+	.join('')}
+</urlset>`;
