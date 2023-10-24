5f8613e2e14de0cd490777391010d6bd3605c667
Sean Behan <codebam@riseup.net>
2023-10-23 21:08:09 -0400

    try using iso date string

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -12,29 +12,29 @@ const render = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url>
 <loc>https://seanbehan.ca</loc>
-<lastmod>${new Date().toUTCString()}</lastmod>
+<lastmod>${new Date().toISOString()}</lastmod>
 </url>
 <url>
 <loc>https://seanbehan.ca/contact</loc>
-<lastmod>${new Date().toUTCString()}</lastmod>
+<lastmod>${new Date().toISOString()}</lastmod>
 </url>
 <url>
 <loc>https://seanbehan.ca/posts</loc>
-<lastmod>${new Date().toUTCString()}</lastmod>
+<lastmod>${new Date().toISOString()}</lastmod>
 </url>
 <url>
 <loc>https://seanbehan.ca/resume</loc>
-<lastmod>${new Date().toUTCString()}</lastmod>
+<lastmod>${new Date().toISOString()}</lastmod>
 </url>
 <url>
 <loc>https://seanbehan.ca/playlists</loc>
-<lastmod>${new Date().toUTCString()}</lastmod>
+<lastmod>${new Date().toISOString()}</lastmod>
 </url>
 ${posts
 	.map(
 		(post) => `<url>
 <loc>https://seanbehan.ca${post.path}</loc>
-<lastmod>${new Date(post.meta.date).toUTCString()}</lastmod>
+<lastmod>${new Date(post.meta.date).toISOString()}</lastmod>
 </url>`
 	)
 	.join('')}
