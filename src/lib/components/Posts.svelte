c53663aec223cac506f45f2741de7493d6f6610e
Sean Behan <codebam@riseup.net>
2023-10-30 20:26:12 -0400

    change theme and add hover effect

--- src/lib/components/Posts.svelte
+++ src/lib/components/Posts.svelte
@@ -6,7 +6,7 @@
 <ul>
   {#if posts}
     {#each posts as post}
-      <li class="m-2 py-4 drop-shadow border-2 border-quaternary dark:border-dark-quaternary rounded-xl">
+      <li class="m-2 py-4 drop-shadow border-2 border-secondary dark:border-dark-secondary rounded-xl hover:border-quaternary hover:dark:border-dark-quaternary">
         <h3 class="underline text-primary dark:text-dark-primary mx-8">
           <a class="hover:text-tertiary dark:hover:text-dark-tertiary" href={post.path}>
             {post.meta.title}
