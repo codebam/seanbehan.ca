dcdfe296960ed68bbbd196bf13f0a2137ede1315
Sean Behan <codebam@riseup.net>
2023-10-30 20:37:57 -0400

    add link hover color

--- src/lib/components/TopBar.svelte
+++ src/lib/components/TopBar.svelte
@@ -6,17 +6,17 @@
   <NavBrand class="mr-8" href="/">
     <Avatar src="/profile.webp"
       class="mr-3" rounded alt="profile" />
-    <span class="self-center whitespace-nowrap text-xl font-semibold text-primary dark:text-dark-primary">Sean Behan</span>
+    <span class="self-center whitespace-nowrap text-xl font-semibold text-primary dark:text-dark-primary hover:text-tertiary hover:dark:text-dark-tertiary">Sean Behan</span>
   </NavBrand>
   <NavUl>
-    <NavLi class="text-primary dark:text-dark-primary" href="/">Home</NavLi>
-    <NavLi class="text-primary dark:text-dark-primary" href="/posts">Posts</NavLi>
-    <NavLi class="text-primary dark:text-dark-primary" href="/playlists">Playlists</NavLi>
-    <NavLi class="text-primary dark:text-dark-primary" href="https://github.com/codebam">GitHub</NavLi>
-    <NavLi class="text-primary dark:text-dark-primary" href="/contact">Contact</NavLi>
-    <NavLi class="text-primary dark:text-dark-primary" href="/resume">Resume</NavLi>
-    <NavLi class="text-primary dark:text-dark-primary" href="https://p.seanbehan.ca">Pastebin</NavLi>
-    <NavLi class="text-primary dark:text-dark-primary" href="https://term.seanbehan.ca">Terminal</NavLi>
+    <NavLi class="text-primary dark:text-dark-primary hover:text-tertiary hover:dark:text-dark-tertiary" href="/">Home</NavLi>
+    <NavLi class="text-primary dark:text-dark-primary hover:text-tertiary hover:dark:text-dark-tertiary" href="/posts">Posts</NavLi>
+    <NavLi class="text-primary dark:text-dark-primary hover:text-tertiary hover:dark:text-dark-tertiary" href="/playlists">Playlists</NavLi>
+    <NavLi class="text-primary dark:text-dark-primary hover:text-tertiary hover:dark:text-dark-tertiary" href="https://github.com/codebam">GitHub</NavLi>
+    <NavLi class="text-primary dark:text-dark-primary hover:text-tertiary hover:dark:text-dark-tertiary" href="/contact">Contact</NavLi>
+    <NavLi class="text-primary dark:text-dark-primary hover:text-tertiary hover:dark:text-dark-tertiary" href="/resume">Resume</NavLi>
+    <NavLi class="text-primary dark:text-dark-primary hover:text-tertiary hover:dark:text-dark-tertiary" href="https://p.seanbehan.ca">Pastebin</NavLi>
+    <NavLi class="text-primary dark:text-dark-primary hover:text-tertiary hover:dark:text-dark-tertiary" href="https://term.seanbehan.ca">Terminal</NavLi>
   </NavUl>
   <NavHamburger />
 </Navbar>
