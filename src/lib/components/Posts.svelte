43ea1306b00847e3710c87c140f70ea72eed95a5
Sean Behan <codebam@riseup.net>
2023-10-31 02:47:30 -0400

    align text better

--- src/lib/components/Posts.svelte
+++ src/lib/components/Posts.svelte
@@ -9,13 +9,13 @@
     {#each posts as post}
       <li class="bg-background-secondary dark:bg-dark-background-secondary m-2
         py-4 pr-4 drop-shadow border-2 border-background-tertiary dark:border-dark-background-tertiary
-        rounded-full hover:border-quaternary hover:dark:border-dark-quaternary">
-        <Heading tag="h5" class="underline text-primary dark:text-dark-primary ml-4">
+        rounded-full hover:border-quaternary hover:dark:border-dark-quaternary w-auto">
+        <Heading tag="h5" class="underline text-primary dark:text-dark-primary ml-8 w-auto">
           <A class="hover:text-tertiary dark:hover:text-dark-tertiary" href={post.path}>
             {post.meta.title}
           </A>
         </Heading>
-        <P class="mx-8 text-secondary dark:text-dark-secondary">
+        <P class="ml-8 text-secondary dark:text-dark-secondary w-auto">
           {new Date(post.meta.date).toDateString()}
         </P>
       </li>
