1ee8785ef7e7565b73ba4d18584c423ba16a10b7
Sean Behan <codebam@riseup.net>
2023-10-31 02:33:13 -0400

    fix overflow in x

--- src/lib/components/Posts.svelte
+++ src/lib/components/Posts.svelte
@@ -3,14 +3,14 @@
   export let posts: {path: string, meta: {title: string, date: string}}[];
 </script>
 
-<Heading tag="h4" class="mx-10 my-4 text-secondary dark:text-dark-secondary">Posts</Heading>
+<Heading tag="h4" class="ml-8 my-4 text-secondary dark:text-dark-secondary w-auto">Posts</Heading>
 <ul>
   {#if posts}
     {#each posts as post}
       <li class="bg-background-secondary dark:bg-dark-background-secondary m-2
         py-4 pr-4 drop-shadow border-2 border-background-tertiary dark:border-dark-background-tertiary
         rounded-full hover:border-quaternary hover:dark:border-dark-quaternary">
-        <Heading tag="h5" class="underline text-primary dark:text-dark-primary mx-8">
+        <Heading tag="h5" class="underline text-primary dark:text-dark-primary ml-4">
           <A class="hover:text-tertiary dark:hover:text-dark-tertiary" href={post.path}>
             {post.meta.title}
           </A>
