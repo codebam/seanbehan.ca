ac7aad58a828c852c0ffd36dd49487e91e694786
Sean Behan <codebam@riseup.net>
2023-10-31 01:07:41 -0400

    change theme to blue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,17 +12,17 @@ const config = {
 			foreground: '#000',
 			background: '#fff',
 			'background-secondary': '#e9ecef',
-			primary: '#583C96',
-			secondary: '#252627',
-			tertiary: '#FF6542',
-			quaternary: '#EF798A',
+			primary: '#145799',
+			secondary: '#191308',
+			tertiary: '#CF5C36',
+			quaternary: '#FBB02D',
 			'dark-background': '#000',
 			'dark-background-secondary': '#212529',
 			'dark-foreground': '#fff',
-			'dark-primary': '#A38AD0',
-			'dark-secondary': '#779FA1',
-			'dark-tertiary': '#FF6542',
-			'dark-quaternary': '#e26d5a'
+			'dark-primary': '#539EE9',
+			'dark-secondary': '#75F4F4',
+			'dark-tertiary': '#EA9E8D',
+			'dark-quaternary': '#FA824C'
 		}
 	}
 };
