de5a1501b2608ba032c28310b7a11035c1b30aef
Sean Behan <codebam@riseup.net>
2023-11-12 23:35:40 -0500

    update bio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,36 +25,14 @@
 	dark:border-dark-background-tertiary rounded-3xl hover:border-quaternary
 	hover:dark:border-dark-quaternary">
 	<P class="mx-4 text-secondary dark:text-dark-secondary my-2">
-		Hi there! My name is Sean Behan, and I'm a full stack developer with a passion for
-		Linux and open source software. With a quick learning mindset and a penchant for
-		problem-solving, I thrive in an environment where I can constantly learn new things and
-		contribute to the greater good of the tech community.
-	</P>
-</div>
-
-<div class="bg-background-secondary dark:bg-dark-background-secondary m-2 p-4
-	drop-shadow border-2 border-background-tertiary
-	dark:border-dark-background-tertiary rounded-3xl hover:border-quaternary
-	hover:dark:border-dark-quaternary">
-	<P class="mx-4 text-secondary dark:text-dark-secondary my-2">
-		When I'm not tinkering with the latest gadgets or experimenting with new programming 
-		languages, you can find me writing software and contributing to open source projects. I
-		love the challenge of working on a variety of projects and collaborating with other 
-		developers to create something truly amazing.
-	</P>
-</div>
-
-<div class="bg-background-secondary dark:bg-dark-background-secondary m-2 p-4
-	drop-shadow border-2 border-background-tertiary
-	dark:border-dark-background-tertiary rounded-3xl hover:border-quaternary
-	hover:dark:border-dark-quaternary">
-	<P class="mx-4 text-secondary dark:text-dark-secondary my-2">
-		With my background in full stack development and my experience with Linux, I'm 
-		confident in my ability to tackle any project that comes my way. Whether it's building 
-		a new web application or diving deep into the inner workings of a complex system, I'm 
-		always up for the challenge. So if you need someone who is quick on their feet and 
-		knows their way around a terminal, look no further! I'm here to help and ready to take 
-		on whatever comes next.
+		As a full stack developer, I am passionate about creating innovative
+		solutions for real-world problems. When I'm not working on client projects,
+		you can find me most of the time on Linux, writing software and
+		contributing to open source projects. With a strong work ethic and a quick
+		learning mindset, I enjoy tackling new challenges and continually improving
+		my skills.  Whether it's learning a new language, exploring new
+		technologies or working on my own personal projects, I am always eager to
+		learn and grow as a developer.
 	</P>
 </div>
 
