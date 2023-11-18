cea919f16060c3e626d61f995d24e0f8e654de20
Sean Behan <codebam@riseup.net>
2023-11-18 17:34:00 -0500

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,34 +2,50 @@
 	import { onMount } from 'svelte';
 	import Posts from '$lib/components/Posts.svelte';
 	import { P } from 'flowbite-svelte';
-	import { initComments } from "$lib/cactus.js";
+	import { initComments } from '$lib/cactus.js';
 
-	onMount(() => initComments({
-				node: document.getElementById("comment-section"),
-				defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
-				serverName: "cactus.chat",
-				siteName: "seanbehan.ca",
-				commentSectionId: "main",
-	}));
+	onMount(() => {
+		initComments({
+			node: document.getElementById('comment-section'),
+			defaultHomeserverUrl: 'https://matrix.cactus.chat:8448',
+			serverName: 'cactus.chat',
+			siteName: 'seanbehan.ca',
+			commentSectionId: 'main'
+		});
+		const source = new EventSource('https://broad-pine-6ec0.codebam.workers.dev/');
+		const el = document.getElementById('bio');
+		el.innerHTML = '';
+		source.onmessage = (event) => {
+			if (event.data === '[DONE]') {
+				source.close();
+				return;
+			}
+			const data = JSON.parse(event.data);
+			el.innerHTML += data.response;
+		};
+	});
 
-	export let data: {posts: {path: string, meta: {title: string, date: string}}[]};
+	export let data: { posts: { path: string; meta: { title: string; date: string } }[] };
 </script>
 
 <svelte:head>
 	<title>Sean Behan</title>
-	<link rel="stylesheet" href="https://latest.cactus.chat/style.css" type="text/css">
+	<link rel="stylesheet" href="https://latest.cactus.chat/style.css" type="text/css" />
 </svelte:head>
 
-<div class="bg-background-secondary dark:bg-dark-background-secondary m-2 p-4
+<div
+	class="bg-background-secondary dark:bg-dark-background-secondary m-2 p-4
 	drop-shadow border-2 border-background-tertiary
 	dark:border-dark-background-tertiary rounded-3xl hover:border-quaternary
-	hover:dark:border-dark-quaternary">
-	<P class="mx-4 text-secondary dark:text-dark-secondary my-2">
-		As a full-stack developer, I specialize in creating and managing web applications using various programming languages
-		and technologies. My primary focus lies within the Linux operating system, where I spend most of my time writing
-		software and contributing to open-source projects. With my aptitude for quick learning, I am always eager to expand my
-		knowledge base and explore new technologies to enhance my skills. This passion for continuous growth enables me to
-		create innovative, efficient, and cutting-edge solutions for a wide range of projects.
+	hover:dark:border-dark-quaternary"
+>
+	<P id="bio" class="mx-4 text-secondary dark:text-dark-secondary my-2">
+		As a full-stack developer, I specialize in creating and managing web applications using various
+		programming languages and technologies. My primary focus lies within the Linux operating system,
+		where I spend most of my time writing software and contributing to open-source projects. With my
+		aptitude for quick learning, I am always eager to expand my knowledge base and explore new
+		technologies to enhance my skills. This passion for continuous growth enables me to create
+		innovative, efficient, and cutting-edge solutions for a wide range of projects.
 	</P>
 </div>
 
