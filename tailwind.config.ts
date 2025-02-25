f54a9ce4ee4152c979df4ca0dbdee0c2c0ebe20d
Sean Behan <codebam@riseup.net>
2025-02-25 15:40:07 -0500

    remove skeleton from tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,13 @@
 import type { Config } from 'tailwindcss';
 import typography from '@tailwindcss/typography';
-import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
-import * as themes from '@skeletonlabs/skeleton/themes';
 
 export default {
   darkMode: 'media',
-  content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
     extend: {}
   },
 
-  plugins: [typography, skeleton({ themes: [themes.catppuccin] })]
+  plugins: [typography]
 } as Config;
