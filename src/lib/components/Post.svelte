3dd8d77b31fa417d112f344c62e20679384e0f48
Sean Behan <codebam@riseup.net>
2025-02-27 01:46:48 -0500

    fix nested lists

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -2,15 +2,13 @@
 	export let post: { path: string; meta: { title: string; date: string } };
 </script>
 
-<ul>
-	<li class="my-2 flex justify-between">
-		<h5>
-			<a href={post.path}>
-				{post.meta.title}
-			</a>
-		</h5>
-		<p>
-			{new Date(post.meta.date).toDateString()}
-		</p>
-	</li>
-</ul>
+<li class="my-2 flex justify-between">
+	<h5>
+		<a href={post.path}>
+			{post.meta.title}
+		</a>
+	</h5>
+	<p>
+		{new Date(post.meta.date).toDateString()}
+	</p>
+</li>
