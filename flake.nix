d93cae29c3e328b4e92edbf79b4815d54ae9cc7f
Sean Behan <codebam@riseup.net>
2025-06-18 03:19:03 -0400

    bump flake lock

--- flake.nix
+++ flake.nix
@@ -2,22 +2,27 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
+  outputs =
     {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          efm-langserver
-          nil
-          nodePackages_latest.nodejs
-          nodePackages_latest.typescript-language-server
-          nodePackages_latest.prettier
-          nodePackages_latest.svelte-language-server
-          vscode-langservers-extracted
-        ];
-      };
-    }
-  );
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodePackages_latest.nodejs
+            nodePackages_latest.typescript-language-server
+            nodePackages_latest.prettier
+            nodePackages_latest.svelte-language-server
+            vscode-langservers-extracted
+          ];
+        };
+      }
+    );
 }
